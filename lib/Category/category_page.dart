b006861572dafac62b3ad43e84ac1fb3e7b619df
AnJW-HGU <22000404@handong.edu>
2021-07-07 15:29:39 +0900

    feat: add ScreenUtil and color variables

--- lib/Category/category_page.dart
+++ lib/Category/category_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'dart:ui';
 
 class CategoryPage extends StatefulWidget {
   const CategoryPage({Key? key}) : super(key: key);
@@ -10,14 +12,17 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Hello!",
-          style: TextStyle(fontFamily: "Barun"),
-        ),
-      )
-
+    return ScreenUtilInit(
+      designSize: Size(411.4, 683.4),
+      builder: () {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Appbar",
+              style: TextStyle(fontFamily: "Barun"),),
+          ),
+        );
+      }
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 import 'package:studytogether/Category/category_page.dart';
 
 final themeColor1 = HexColor("#0080FF");
+final themeColor2 = HexColor("#03A6FF");
+final themeColor3 = HexColor("#A3DAFF");
+final themeColor4 = HexColor("#DAF0FF");
+
+final blurColor = Colors.black.withOpacity(0.25);
+
+final grayColor1 = HexColor("5C5C5C");
+final grayColor2 = HexColor("ADADAD");
 
 void main() {
   runApp(
@@ -18,7 +27,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       home: CategoryPage(),
     );
   }
