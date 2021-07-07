b7aedd96960376d5315ed9cbaab1d8ac502d549f
AnJW-HGU <22000404@handong.edu>
2021-07-07 17:08:57 +0900

    feat: Add google-sign-in pub and categoryHome_page
    We decided to log-in with a Google account. (Token Type)
    The categoryHome_page is required because the categoty_page uses expasion
    tile.

--- lib/Category/category_page.dart
+++ lib/Category/category_page.dart
@@ -2,27 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'dart:ui';
 
-class CategoryPage extends StatefulWidget {
-  const CategoryPage({Key? key}) : super(key: key);
+class CategoryHomePage extends StatefulWidget {
+  const CategoryHomePage({Key? key}) : super(key: key);
 
   @override
-  _CategoryPageState createState() => _CategoryPageState();
+  _CategoryHomePageState createState() => _CategoryHomePageState();
 }
 
-class _CategoryPageState extends State<CategoryPage> {
+class _CategoryHomePageState extends State<CategoryHomePage> {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: Size(411.4, 683.4),
-      builder: () {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text(
-              "Appbar",
-              style: TextStyle(fontFamily: "Barun"),),
-          ),
-        );
-      }
+        designSize: Size(411.4, 683.4),
+        builder: () {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(
+                  "Appbar"
+              ),
+            ),
+          );
+        }
     );
   }
 }
