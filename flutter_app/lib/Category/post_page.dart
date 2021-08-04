c6698190d34d8c1da31e759bd0925bb2464b6873
AnJW-HGU <22000404@handong.edu>
2021-08-04 16:43:20 +0900

    feat: Add future builder

--- flutter_app/lib/Category/post_page.dart
+++ flutter_app/lib/Category/post_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
@@ -10,17 +11,23 @@ import 'dart:math';
 
 import 'package:studytogether/main.dart';
 
-class PostPage extends StatefulWidget {
-  const PostPage({Key? key}) : super(key: key);
+Future<Post> fetchPost() async {
+  String postUrl =
+      "https://c64ab34d-ad62-4f6e-9578-9a43e222b9bf.mock.pstmn.io/post?post_id=000001";
+  var response = await http.get(Uri.parse(postUrl));
 
-  @override
-  _PostPageState createState() => _PostPageState();
+  if (response.statusCode == 200) {
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    throw Exception("Failed to load post");
+  }
 }
 
 // Post에 대한 Data
 class Post {
   var post_sub;
   var post_userId;
+  var post_userName;
   var post_title;
   var post_content;
   var post_comments_count;
@@ -31,6 +38,7 @@ class Post {
   Post({
     this.post_sub,
     this.post_userId,
+    this.post_userName,
     this.post_title,
     this.post_content,
     this.post_comments_count,
@@ -43,6 +51,7 @@ class Post {
     return Post(
       post_sub: json["subject"],
       post_userId: json["user_id"],
+      post_userName: json["user_name"],
       post_title: json["title"],
       post_content: json["content"],
       post_comments_count: json["comments_count"],
@@ -53,6 +62,14 @@ class Post {
   }
 }
 
+// UI
+class PostPage extends StatefulWidget {
+  const PostPage({Key? key}) : super(key: key);
+
+  @override
+  _PostPageState createState() => _PostPageState();
+}
+
 class _PostPageState extends State<PostPage> {
   late Future<Post> post;
 
@@ -61,19 +78,9 @@ class _PostPageState extends State<PostPage> {
     super.initState();
     post = fetchPost();
   }
-
-  Future<Post> fetchPost() async {
-    String postUrl = "https://c64ab34d-ad62-4f6e-9578-9a43e222b9bf.mock.pstmn.io/post?post_id=000001";
-    var response = await http.get(Uri.parse(postUrl));
-
-    if (response.statusCode == 200) {
-      return Post.fromJson(json.decode(response.body));
-    } else {
-      throw Exception("Failed to load post");
-    }
-  }
   /*
   게시글에서 필요한 것
+  아이콘... 질문자와 비교해서...
   게시글 refresh
     1. 댓글 또는 대댓글 달면 refresh
     2. 신고?
@@ -117,134 +124,272 @@ class _PostPageState extends State<PostPage> {
             actions: [
               IconButton(
                 color: grayColor2,
-                icon: Icon(Icons.warning_rounded,),
+                icon: Icon(
+                  Icons.warning_rounded,
+                ),
                 tooltip: "Declaration Button",
                 iconSize: 25.w,
-                onPressed: () {
-
-                },
+                onPressed: () {},
               )
             ],
           ),
-
-          body: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.only(top: 15.w, left: 30.w, right: 30.w, bottom: 20.w),
-                child: Column(
+          body: FutureBuilder<Post>(
+            future: post,
+            builder: (context, snapshot) {
+              if (snapshot.data != null) {
+                return Column(
                   children: [
-
-                    // 프로필과 닉네임
-                    Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-
-                          },
-                          child: CircleAvatar(
-                            radius: 20,
-                            backgroundColor: themeColor4,
-                          ),
-                        ),
-
-                        Padding(
-                          padding: EdgeInsets.only(left: 10.w),
-                          child: Text(
-                            "닉네임",
-                            style: TextStyle(
-                              color: themeColor2,
-                              fontFamily: "Barun",
-                              fontSize: 15.sp,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-
-                    // 질문 제목과 질문 내용
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(top: 20),
-                          child: Text(
-                            "질문 제목",
-                            style: TextStyle(
-                              fontFamily: "Barun",
-                              fontSize: 15.sp,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ),
-
-                        Padding(
-                          padding: EdgeInsets.only(top: 20),
-                          child: Text(
-                            "질문 내용은 이곳에 나타났습니다.\n"
-                            "질문 내용\n"
-                            "질문 내용\n",
-                            style: TextStyle(
-                              fontFamily: "Barun",
-                              fontSize: 15.sp,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(15),
-                      bottomRight: Radius.circular(15),
-                    ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: blurColor,
-                      blurRadius: 4,
-                      offset: Offset(0, 2),
-                    )
-                  ],
-                ),
-              ),
-
-              // 댓글
-              Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.only(top:20, left: 15.w, right: 15.w),
-                    child: Container(
-                      padding: EdgeInsets.only(top: 15.w, left: 30.w, right: 30.w, bottom: 20.w),
+                    Container(
+                      padding: EdgeInsets.only(
+                          top: 0.h, left: 30.w, right: 30.w, bottom: 25.h),
                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
-                          //댓글들...
-                          Text("Hi"),
+                          // 프로필과 닉네임
+                          Row(
+                            children: [
+                              GestureDetector(
+                                onTap: () {},
+                                child: CircleAvatar(
+                                  radius: 20,
+                                  backgroundColor: themeColor4,
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 10.w),
+                                child: Text(
+                                  snapshot.data!.post_userName,
+                                  style: TextStyle(
+                                    color: themeColor2,
+                                    fontFamily: "Barun",
+                                    fontSize: 15.sp,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          // 질문 제목과 질문 내용
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(top: 20.h),
+                                child: Text(
+                                  snapshot.data!.post_title,
+                                  style: TextStyle(
+                                    fontFamily: "Barun",
+                                    fontSize: 15.sp,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 10.h),
+                                child: Text(
+                                  snapshot.data!.post_content,
+                                  style: TextStyle(
+                                    fontFamily: "Barun",
+                                    fontSize: 15.sp,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+
+
+                          Padding(
+                            padding: EdgeInsets.only(top: 15.h, left: 0, right: 0, bottom: 0),
+                          ),
+                          // 날짜와 댓글 총 수
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(top: 0, left: 0, right: 0, bottom: 0),
+                                child: Text(
+                                  snapshot.data!.post_writtenDate,
+                                  style: TextStyle(
+                                    color: grayColor1,
+                                    fontFamily: "barun",
+                                    fontSize: 12.sp,
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                              ),
+
+                              Padding(
+                                padding:EdgeInsets.all(0),
+                                child: Text(
+                                  snapshot.data!.post_comments_count.toString(),
+                                  style: TextStyle(
+                                    color: themeColor2,
+                                    fontFamily: "barun",
+                                    fontSize: 12.sp,
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ],
                       ),
-
                       decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(15),
+                          bottomRight: Radius.circular(15),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: blurColor,
+                            blurRadius: 4,
+                            offset: Offset(0, 2),
+                          )
+                        ],
+                      ),
+                    ),
+
+                    // 댓글
+                    Expanded(
+                      child: Padding(
+                        padding:
+                            EdgeInsets.only(top: 20, left: 15.w, right: 15.w),
+                        child: Container(
+                          padding: EdgeInsets.only(
+                              top: 15.w, left: 30.w, right: 30.w, bottom: 20.w),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              //댓글들...
+                              Text("Hi"),
+                            ],
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              topRight: Radius.circular(10),
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: blurColor,
+                                blurRadius: 4,
+                                offset: Offset(2, 0),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              return Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(
+                        top: 0.h, left: 30.w, right: 30.w, bottom: 25.h),
+                    child: Column(
+                      children: [
+                        // 프로필과 닉네임
+                        Row(
+                          children: [
+                            GestureDetector(
+                              onTap: () {},
+                              child: CircleAvatar(
+                                radius: 20,
+                                backgroundColor: themeColor4,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 10.w),
+                              child: Container(
+                                width: 100.w,
+                                height: 16.h,
+                                color: themeColor4,
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        // 질문 제목과 질문 내용
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(top: 20.h),
+                              child: Container(
+                                height: 16.h,
+                                color: grayColor2,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(top: 10.h),
+                              child: Container(
+                                height: 30.h,
+                                color: grayColor2,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(15),
+                        bottomRight: Radius.circular(15),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: blurColor,
+                          blurRadius: 4,
+                          offset: Offset(0, 2),
+                        )
+                      ],
+                    ),
+                  ),
+
+                  // 댓글
+                  Expanded(
+                    child: Padding(
+                      padding:
+                      EdgeInsets.only(top: 20, left: 15.w, right: 15.w),
+                      child: Container(
+                        padding: EdgeInsets.only(
+                            top: 15.w, left: 30.w, right: 30.w, bottom: 20.w),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            //댓글들...
+                            Text("Hi"),
+                          ],
+                        ),
+                        decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.only(
                             topLeft: Radius.circular(10),
                             topRight: Radius.circular(10),
                           ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: blurColor,
-                            blurRadius: 4,
-                            offset: Offset(2, 0),
-                          )
-                        ],
+                          boxShadow: [
+                            BoxShadow(
+                              color: blurColor,
+                              blurRadius: 4,
+                              offset: Offset(2, 0),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
-              ),
-
-            ],
+                ],
+              );
+            },
           ),
         );
       },
