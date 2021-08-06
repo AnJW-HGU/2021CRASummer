3a81078db4f2e0976ee27b1b9b112b7a2c302bf0
Saon227 <22000050@handong.edu>
2021-08-06 14:48:21 +0900

    feat: Finish splash_page

--- flutter_app/lib/splash_page.dart
+++ flutter_app/lib/splash_page.dart
@@ -12,13 +12,14 @@ import 'package:http/http.dart' as http;
 
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 
+import 'Login/login_page.dart';
+
 Future<Splash> fetchSplash() async {
-  var response = await http.get(Uri.parse('https://0c5f29c1-526c-4b53-af83-ce20fecb0819.mock.pstmn.io/noti?user_id=020202'));
+  var response = await http.get(Uri.parse('https://0c5f29c1-526c-4b53-af83-ce20fecb0819.mock.pstmn.io/splash?user_id=000000'));
 
   if (response.statusCode == 200) {
     // 만약 서버로의 요청이 성공하면, JSON을 파싱합니다.
-    print("a");
-    return Splash.fromJson(json.decode(response.body));
+    return await Future.delayed(Duration(seconds: 1), () => Splash.fromJson(json.decode(response.body)));
   } else {
     // 만약 요청이 실패하면, 에러를 던집니다.
     throw Exception('Failed to load post');
@@ -39,7 +40,6 @@ class Splash {
   }
 }
 
-
 class SplashPage extends StatefulWidget {
   @override
   _SplashPageState createState() => _SplashPageState();
@@ -51,7 +51,7 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _animation;
 
-  bool _isLoggedIn = false;
+  //bool _isLoggedIn = false;
 
   void initState() {
     _controller = AnimationController(
@@ -63,23 +63,22 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
     _controller.forward();
     super.initState();
     splash = fetchSplash();
-
-
-  }
-
-  Future MainPage() async {
-    await Future.delayed(Duration(seconds: 3), () => Navigator.pop(context));
-    return Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) =>
-            _isLoggedIn ? CategoryPage() : CategoryPage()));
   }
 
   @override
   void dispose() {
-    _controller.dispose();
     super.dispose();
+    _controller.dispose();
+  }
+
+  Future MainPage(bool _isLoggedIn) async {
+    await Future.delayed(Duration(seconds: 0), () => Navigator.pop(context));
+    return Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) =>
+            _isLoggedIn ? CategoryPage() : LoginPage()
+        ));
   }
 
   @override
@@ -97,8 +96,8 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
 
   Widget _splashPageBody() {
     return Scaffold(
-      body: FutureBuilder(
-        future: MainPage(),
+      body: FutureBuilder<Splash>(
+        future: splash,
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (snapshot.hasData == false) {
             return Column(
@@ -120,11 +119,12 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
             );
           } else if (snapshot.hasError) {
             return Text('에러');
-          } else
+          } else{
+            MainPage(snapshot.data!.splash_is_logined);
             return Center();
+          }
         },
       ),
     );
   }
-
 }
