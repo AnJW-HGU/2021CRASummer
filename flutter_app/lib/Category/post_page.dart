57dce6a795023372cee865fefeb05464fcdd656c
AnJW-HGU <22000404@handong.edu>
2021-08-11 11:53:28 +0900

    feat: Create an infinite scroll with data
    I implemented building with infinite scrolls after importing Commets' data in http. Because it is to shorten the time to build on the bottom. If app import more data in the future, I will insert the circular progress indicator.

--- flutter_app/lib/Category/post_page.dart
+++ flutter_app/lib/Category/post_page.dart
@@ -11,7 +11,7 @@ import 'dart:ui';
 import 'package:studytogether/main.dart';
 
 
-//POST
+//POST의 데이터 가져오기
 Future<Post> fetchPost() async {
   final post_id = Get.arguments.toString();
   // Map<String, dynamic> queryParams = {
@@ -19,7 +19,7 @@ Future<Post> fetchPost() async {
   // };
   //
   // String queryString = Uri(queryParameters: queryParams).query;
-  var postUrl = "https://c64ab34d-ad62-4f6e-9578-9a43e222b9bf.mock.pstmn.io/post"+"/"+post_id;
+  var postUrl = "https://c64ab34d-ad62-4f6e-9578-9a43e222b9bf.mock.pstmn.io/post/"+post_id;
   var response = await http.get(Uri.parse(postUrl));
 
   if (response.statusCode == 200) {
@@ -29,7 +29,7 @@ Future<Post> fetchPost() async {
   }
 }
 
-// Post에 대한 Data
+// Post에 대한 Data형식
 class Post {
   var post_id;
   var post_sub;
@@ -72,7 +72,7 @@ class Post {
 }
 
 
-//Comment
+//Comment 작성
 _AddComment(inPostId, inUserId, inContent) async {
   String url = "https://c64ab34d-ad62-4f6e-9578-9a43e222b9bf.mock.pstmn.io/Comment/";
   AddComment _addComment = AddComment(inPostId, inUserId, inContent);
@@ -97,6 +97,7 @@ apiRequest(url, _addComment) async {
   return reply;
 }
 
+// Comment 작성 시 데이터 형식
 class AddComment {
   var comment_post_id;
   var comment_user_id;
@@ -117,15 +118,38 @@ class AddComment {
       };
 }
 
+// Comment 데이터 가져오기
+List<Comment> CommentfromJson (json) {
+  List<Comment> result = [];
+  json.forEach((item){
+    result.add(Comment(item["user_id"], item["nickname"], item['content'], item["recommends_count"], item["written_date"], item["adopted_status"]));
+  });
 
-// Comment
+  return result;
+}
+
+Future<List<Comment>> fetchComment() async {
+  final post_id = Get.arguments.toString();
+  var commentUrl = "https://c64ab34d-ad62-4f6e-9578-9a43e222b9bf.mock.pstmn.io/comments"+"?postId="+post_id;
+  // var commentUrl = "https://c64ab34d-ad62-4f6e-9578-9a43e222b9bf.mock.pstmn.io/comments?postId=1";
+  var response = await http.get(Uri.parse(commentUrl));
+
+
+  if (response.statusCode == 200) {
+    return CommentfromJson(json.decode(response.body));
+  } else {
+    throw Exception("Failed to load comment");
+  }
+}
+
+// Comment 가져올 때 데이터 형식
 class Comment{
   var comment_userId;
   var comment_nickName;
   var comment_content;
   var comment_recommendCount;
   var comment_writtenDate;
-  var comment_apoptedStatus;
+  var comment_adoptedStatus;
 
   Comment(
     this.comment_userId,
@@ -133,7 +157,7 @@ class Comment{
     this.comment_content,
     this.comment_recommendCount,
     this.comment_writtenDate,
-    this.comment_apoptedStatus,
+    this.comment_adoptedStatus,
   );
 
   // Comment(inUserId, inNick, inContent, inRecommend, inWritten, inAdopted) {
@@ -157,38 +181,6 @@ class Comment{
   // }
 }
 
-List<Comment> CommentfromJson (json) {
-  print("a");
-  List<Comment> result = [];
-  // for (int i=0; i<json.length; i++) {
-  //   Comment _comment = Comment.fromJson(json[i]);
-  //   result.add(_comment);
-  // }
-  json.forEach((item){
-    result.add(Comment(item["user_id"], item["nickname"], item['content'], item["recommends_count"], item["written_date"], item["adopted_status"]));
-  });
-  // List<Comment> result = json.map((item) => Comment(item["user_id"], item["nickname"], item['content'], item["recommends_count"], item["written_date"], item["adopted_status"])).toList();
-  return result;
-}
-
-Future<List<Comment>> fetchComment() async {
-  final post_id = "1";
-  // var commentUrl = "https://c64ab34d-ad62-4f6e-9578-9a43e222b9bf.mock.pstmn.io/comments"+"?postId="+post_id;
-  var commentUrl = "https://c64ab34d-ad62-4f6e-9578-9a43e222b9bf.mock.pstmn.io/comments?postId=1";
-  var response = await http.get(Uri.parse(commentUrl));
-
-
-  if (response.statusCode == 200) {
-    print("b");
-    //print(response.body);
-    var body = json.decode(response.body.toString());
-    print("c");
-    print(body);
-    return CommentfromJson(body);
-  } else {
-    throw Exception("Failed to load comment");
-  }
-}
 
 
 ///////////////////////////////////////////////////////////////////
@@ -204,7 +196,7 @@ class PostPage extends StatefulWidget {
 
 class _PostPageState extends State<PostPage> {
   late Future<Post> post;
-  List<Comment> _commentDataList = [];
+  List<Comment> _commentDataList = <Comment>[].obs;
 
   final userId=1;
 
@@ -339,6 +331,7 @@ class _PostPageState extends State<PostPage> {
                     Expanded(
                       child: SingleChildScrollView(
                         scrollDirection: Axis.vertical,
+                        controller: _scroll.value,
                         child: Column(
                           children: [
                             // 배경? == 질문+댓글
@@ -492,6 +485,9 @@ class _PostPageState extends State<PostPage> {
                             // 댓글 관련
                             Container(
                               width: 450.w,
+                              constraints: BoxConstraints(
+                                minHeight: 400.h,
+                              ),
                               child: Padding(
                                 padding:
                                 EdgeInsets.only(top: 20.h, bottom: 20.h, left: 15.w, right: 15.w),
@@ -502,21 +498,59 @@ class _PostPageState extends State<PostPage> {
                                     crossAxisAlignment: CrossAxisAlignment.stretch,
                                     children: [
                                       for (int i=0; i<_commentDataList.length; i++) Container(
-                                        child: Row(
+                                        child: Column(
                                           children: [
-                                            GestureDetector(
-                                              onTap: () {},
-                                              child: CircleAvatar(
-                                                radius: 20,
-                                                backgroundColor: themeColor4,
-                                              ),
+                                            // 프로필 & 닉네임 & 채택
+                                            Row(
+                                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                              children: [
+
+                                                // 프로필 & 닉네임
+                                                Row(
+                                                  children: [
+                                                    GestureDetector(
+                                                      onTap: () {},
+                                                      child: CircleAvatar(
+                                                        radius: 20,
+                                                        backgroundColor: themeColor4,
+                                                      ),
+                                                    ),
+
+                                                    Padding(
+                                                      padding: EdgeInsets.only(left: 10.w),
+                                                      child: Text(
+                                                        _commentDataList[i].comment_nickName,
+                                                        style: TextStyle(
+                                                          color: Colors.black,
+                                                          fontFamily: "Barun",
+                                                          fontSize: 15.sp,
+                                                          fontWeight: FontWeight.w400,
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+
+                                                // 채택
+                                                if (_commentDataList[i].comment_adoptedStatus == true) IconButton(
+                                                  onPressed: () {
+                                                  },
+                                                  tooltip: "Adopted Button",
+                                                  icon: Icon(
+                                                    Icons.check_rounded,
+                                                    color: themeColor2,
+                                                  ),
+                                                  alignment: Alignment.centerRight,
+                                                ),
+                                              ],
                                             ),
-                                            Padding(
-                                              padding: EdgeInsets.only(left: 10.w),
+
+                                            // 댓글 내용
+                                            Container(
+                                              alignment: Alignment.centerLeft,
                                               child: Text(
-                                                _commentDataList[i].comment_nickName,
+                                                _commentDataList[i].comment_content,
                                                 style: TextStyle(
-                                                  color: Colors.black,
                                                   fontFamily: "Barun",
                                                   fontSize: 15.sp,
                                                   fontWeight: FontWeight.w400,
@@ -525,62 +559,7 @@ class _PostPageState extends State<PostPage> {
                                             ),
                                           ],
                                         ),
-                                      )
-
-                                        //댓글들...
-                                        // Obx(()
-                                        //   => ListView.separated(
-                                        //     controller: _scroll.value,
-                                        //     itemBuilder: (_, index) {
-                                        //       print(_hasMoreComment.value);
-                                        //
-                                        //       if (index < _commentDataList.length) {
-                                        //           return Container(
-                                        //             child: Row(
-                                        //               children: [
-                                        //                 GestureDetector(
-                                        //                   onTap: () {},
-                                        //                   child: CircleAvatar(
-                                        //                     radius: 20,
-                                        //                     backgroundColor: themeColor4,
-                                        //                   ),
-                                        //                 ),
-                                        //                 Padding(
-                                        //                   padding: EdgeInsets.only(left: 10.w),
-                                        //                   child: Text(
-                                        //                     _commentDataList[index].comment_nickName,
-                                        //                     style: TextStyle(
-                                        //                       color: Colors.black,
-                                        //                       fontFamily: "Barun",
-                                        //                       fontSize: 15.sp,
-                                        //                       fontWeight: FontWeight.w400,
-                                        //                     ),
-                                        //                   ),
-                                        //                 ),
-                                        //               ],
-                                        //             ),
-                                        //           );
-                                        //       }
-                                        //       else if (_hasMoreComment.value || _isLoading.value) {
-                                        //         return Center(
-                                        //           child: CircularProgressIndicator(),
-                                        //         );
-                                        //       }
-                                        //
-                                        //       return Container(
-                                        //         child: IconButton(
-                                        //           onPressed: () {
-                                        //             print("성공!");
-                                        //           },
-                                        //           icon: Icon(Icons.arrow_upward_outlined),
-                                        //         ),
-                                        //       );
-                                        //     },
-                                        //     separatorBuilder: (_, index) => Divider(),
-                                        //     itemCount: _commentDataList.length + 1,
-                                        //   ),
-                                        // )
-
+                                      ),
                                     ],
                                   ),
                                   decoration: BoxDecoration(
@@ -687,11 +666,4 @@ class _PostPageState extends State<PostPage> {
       },
     );
   }
-
-  // Widget _MakeComment(cUserId, cNickname, cContent, cWrittenDate, cAdopted) {
-  //   return Padding(
-  //     padding: EdgeInsets.all(0),
-  //     child: Col,
-  //   );
-  // }
 }
