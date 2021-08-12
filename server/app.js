ebc12d7ab682e4cce6be90f4ef0bb4d65f5923ed
seohyunseo <21700358@handong.edu>
2021-08-12 16:13:26 +0900

    feature: cla_test 모델 추가, 과목 업데이트 기능 추가
    cla_test 모델, 마이그레이션 코드 추가
    과목 모델로 업데이트 하는 기능 추가 - 오류나서 해결

--- server/app.js
+++ server/app.js
@@ -3,14 +3,14 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
 const { sequelize } = require('./models');
-const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/config/config.json');
-
+const config = require(__dirname + '/config/config.json')[env];
+const passport = require('passport');
 // maybe login ?
 // const secretKey = require('./config/jwt_secret');
 // const passport = require('passport');
@@ -25,9 +25,15 @@ sequelize.sync();
 
 // define router var
 const app = express();
-const router = require('./routes');
 
-// session store
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(cookieParser('asdfghjkl'));
+
 const sessionStore = new MySQLStore({
     host: config.host,
     port: config.port,
@@ -36,19 +42,30 @@ const sessionStore = new MySQLStore({
     database: config.database
 });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const grabController = require('./controllers/grab');
+grabController.main();
 
-app.use(cors());
-app.use(logger('dev'));
+app.use(session({
+	secret: "asdfghjkl",
+	resave: false,
+	saveUninitialized: true,
+	store: sessionStore,
+	cookie: {
+		secure: false,
+		httpOnly: false,
+		maxAge: 1000 * 60 * 2
+	}
+}));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-
+const router = require('./routes');
 app.use('/', router);
+
 // TODO: initialize express-session to allow us track the logged-in user across sessions.
 
 // use router var
@@ -56,17 +73,7 @@ app.use('/', router);
 //app.use('/users', userRouter);
 
 // catch 404 and forward to error handler
-app.use(session({
-	secret: "asdfghjkl",
-	resave: false,
-	saveUninitialized: false,
-	store: sessionStore,
-	cookie: {
-		secure: false,
-		httpOnly: true,
-		maxAge: 1000 * 60 * 2
-	}
-}));
+
 
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ server/models/cla_test.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Cla_test extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Cla_test.init({
+    구분: DataTypes.STRING,
+    과목코드: DataTypes.STRING,
+    분반: DataTypes.STRING,
+    과목명: DataTypes.STRING,
+    학점: DataTypes.STRING,
+    개설정보: DataTypes.STRING,
+    시간: DataTypes.STRING,
+    강의실: DataTypes.STRING,
+    정원: DataTypes.STRING,
+    인원: DataTypes.STRING,
+    영어: DataTypes.STRING,
+    교양: DataTypes.STRING,
+    성적유형: DataTypes.STRING,
+    PF병행: DataTypes.STRING,
+    강의: DataTypes.STRING,
+    비고: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Cla_test',
+  });
+  return Cla_test;
+};
